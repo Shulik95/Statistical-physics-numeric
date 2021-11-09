d56979d377b5594617c963e73ff90f3d1143fff5
Shulik <kachko,shulik@gmail.com>
2021-11-09 16:25:39 +0200

    Particle class done, starting sim class

--- Ex2/MolDynamics.py
+++ Ex2/MolDynamics.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 from numpy.linalg import norm
 from matplotlib import pyplot as plt
@@ -19,12 +21,58 @@ class Particle:
         """
         self.pos = self.pos + self.vel * dt
 
-    def distance_to_particle(self, other):
+    def time_to_particle(self, other):
         """
-        return distance from this Particle to other Particle.
+        return time until Particle to other Particle.
         :param other: 2nd Particle.
+        """
+        deltal = other.pos - self.pos
+        deltav = other.vel - self.vel
+        s = np.dot(deltav, deltal)
+        det = s ** 2 - (norm(deltav) ** 2) * (norm(deltal) ** 2 - 4 * (self.rad ** 2))
+        if det > 0 > s:
+            return -(s + np.sqrt(det)) / (norm(deltav) ** 2)
+        else:
+            return math.inf
+
+    def time_to_wall(self):
+        """
+        returns distance
+        :return:
+        """
+        vx, vy = self.vel
+        dtwall_x = (1 - self.rad) / vx if vx > 0 else self.rad / abs(vx)
+        dtwall_y = (1 - self.rad) / vy if vy > 0 else self.rad / abs(vy)
+        return min(dtwall_y, dtwall_x)
+
+    def get_speed(self):
+        """
+        returns the speed of the current disc.
+        """
+        return norm(self.vel)
+
+
+class Simulation:
+    """simulation of circular particles in motion"""
+
+    def __init__(self, v_table, p_table, nparticles=4):
+        self.v_table = v_table
+        self.p_table = p_table
+        self.particles = [self.init_particles(p_table, v_table, i) for i in range(nparticles)]
+        self.x = np.linspace()
+
+    def init_particles(self, p_table, v_table, row):
+        """
+        return a new Particle object with position and velocity according
+        to tables.
+        :param p_table: table of size 4x2 where each row is the x&y position of
+        the particle corresponding to that row.
+        :param v_table: table of size 4x2 where each row is the vx&vy of
+        the particle corresponding to that row.
         """
         pass
 
-    def get_speed(self):
-        return norm(self.vel)
+
+if __name__ == '__main__':
+    p_table = np.array([[0.25, 0.25], [0.25, 0.75], [0.75, 0.25], [0.75, 0.75]])
+    v_table = np.array([[0.21, 0.12], [0.71, 0.18], [-0.23, -0.79], [0.78, 0.34583]])
