103c254b760fb300d656dbe598e6ccfc5d6b51b6
Shulik <kachko,shulik@gmail.com>
2021-11-28 09:53:41 +0200

    starting project - first push

---
+++ Ex3/Numeric_Ex3.py
@@ -0,0 +1,6 @@
+# ------------------------------ imports ------------------------------
+import numpy as np
+from matplotlib import pyplot
+import math
+
+# ------------------------------- code --------------------------------
